7b6497b6282d22848335b77a524c463f781b1e65
gdtGH <gdt.giacomo@gmail.com>
2025-05-18 22:26:16 +0200

    Aggiornamenti notine

--- GAIA_Arduino/GAIA_Arduino.ino
+++ GAIA_Arduino/GAIA_Arduino.ino
@@ -15,14 +15,18 @@ const char* passList[] = {"GDThotspot", "benvenuti!"};
 const int numNetworks = 2;
 
 IPAddress remoteIP(192, 168, 123, 50);
-const unsigned int remotePort = 57120;
+unsigned int remotePort[] = {57120, 57121};
 
 int temp = 0;
 int humi = 0;
 int rldr = 0;
 int triggerBar = 0;
 unsigned long durBar = random(5000, 15000);
+int triggerNote = 0;
+unsigned long durNote = random(1500, 3000);
 unsigned long barStart = 0;
+unsigned long noteStart = 0;
+unsigned int note = random(0, 12);
 
 void connectToWiFi() {
   for (int i = 0; i < numNetworks; i++) {
@@ -43,6 +47,7 @@ void setup() {
   connectToWiFi();
   Udp.begin(8888);
   barStart = millis();
+  noteStart = millis();
 }
 
 void loop() {
@@ -57,25 +62,46 @@ void loop() {
     triggerBar = 0;
   }
 
+  if (now - noteStart >= durNote) {
+    triggerNote = 1;
+    durNote = random(1500, 3000);  
+    note = random(0, 12);
+    noteStart = now;               
+  } else {
+    triggerNote = 0;
+  }
+
   // === Lettura sensori ===
   temp = 100 * dht.readTemperature();
   humi = 100 * dht.readHumidity();
   rldr = analogRead(RLDR_PIN);
 
   // === Invio dati OSC ===
-  sendOSC("/sensors/temp", temp);
-  sendOSC("/sensors/humi", humi);
-  sendOSC("/sensors/rldr", rldr);
-  sendOSC("/trigger/tbar", triggerBar);
-  sendOSC("/duratab/dbar", durBar);
-
+  sendOSC("/sensors/temp", temp, remotePort[0]);
+  sendOSC("/sensors/humi", humi, remotePort[0]);
+  sendOSC("/sensors/rldr", rldr, remotePort[0]);
+  sendOSC("/trigger/tbar", triggerBar, remotePort[0]);
+  sendOSC("/duratat/dbar", durBar, remotePort[0]);
+  sendOSC("/trigger/tnot", triggerNote, remotePort[0]);
+  sendOSC("/duratat/dnot", durNote, remotePort[0]);
+  sendOSC("/musnote/note", note, remotePort[0]);
+  /*
+  sendOSC("/sensors/temp", temp, remotePort[1]);
+  sendOSC("/sensors/humi", humi, remotePort[1]);
+  sendOSC("/sensors/rldr", rldr, remotePort[1]);
+  sendOSC("/trigger/tbar", triggerBar, remotePort[1]);
+  sendOSC("/duratab/dbar", durBar, remotePort[1]);
+  sendOSC("/trigger/tnot", triggerNote, remotePort[1]);
+  sendOSC("/duratat/dnot", durBar, remotePort[1]);
+  sendOSC("/musnote/note", note, remotePort[1]);
+  */
   delay(200);
 }
 
-void sendOSC(const char* address, int value) {
+void sendOSC(const char* address, int value, unsigned int port) {
   OSCMessage msg(address);
   msg.add(value);
-  Udp.beginPacket(remoteIP, remotePort);
+  Udp.beginPacket(remoteIP, port);
   msg.send(Udp);
   Udp.endPacket();
   msg.empty();
