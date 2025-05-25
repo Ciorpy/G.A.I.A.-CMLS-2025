5d9182d7a72f6230b90e5ddb77eef77fb469fad3
Marco-Porcella <59924217+Ciorpy@users.noreply.github.com>
2025-05-25 15:29:23 +0200

    JUCE Comments

--- GAIA_Juce/Source/PluginEditor.cpp
+++ GAIA_Juce/Source/PluginEditor.cpp
@@ -16,7 +16,11 @@ GAIAJuceAudioProcessorEditor::GAIAJuceAudioProcessorEditor (GAIAJuceAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    
+    //Setup UI
     audioProcessor.setupMixerUI([this](juce::Component& comp) { addAndMakeVisible(comp); });
+
+    // Each block is 200 x 250 pixels
     setSize (600, 750);
 }
 
@@ -31,10 +35,12 @@ void GAIAJuceAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
+    // UI Separators
 	g.setColour(juce::Colours::grey); 
     g.drawHorizontalLine(250, 0, getWidth());
     g.drawHorizontalLine(500, 0, getWidth());
 
+    // Title
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (40.0f));
     g.drawFittedText ("G.A.I.A.", getLocalBounds(), juce::Justification::centredBottom, 1);
